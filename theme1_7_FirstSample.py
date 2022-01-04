11483a03d52042568f15b8a35c302ab192ef9e23
paraches <paraches+github@gmail.com>
2022-01-04 19:05:23 +0900

    calc theme1-5 implemented

--- theme1_7_FirstSample.py
+++ theme1_7_FirstSample.py
@@ -45,7 +45,30 @@ def theme1_7_first_sample():
             pw = PVector(map_range(x, 0, width - 1, -1, 1), map_range(y, 0, height - 1, -1, 1), 0)
             de = pw.sub(pe)
 
-            screen.set_at((x, y), background_color)
+            # chapter 3
+            # theme 1-5
+            #
+            # |p - pc|^2 = r^2
+            # p = s + td
+            # |s + td - pc|^2 = r^2
+            # |s - pc + td|^2 = r^2
+            # |be + td|^2 = r^2   be = s - pc (ball eye)
+            # |be|^2 + 2(be dot d)t + |d|^2t^2 = r^2
+            # |d|^2t^2 + 2(be dot d)t + |be|^2 - r^2 = 0
+            # A = |d|^2, B = 2(be dot d), C = |be|^2 - r^2
+            # t = -B +/- sqrt(B^2 - 4AC) / 2A
+            be = pe.sub(pc)
+            a = de.mag_sq()
+            b = 2 * be.dot(de)
+            c = be.mag_sq() - r ** 2
+            d = b ** 2 - 4 * a * c
+
+            if d >= 0:
+                color = ball_color
+            else:
+                color = background_color
+
+            screen.set_at((x, y), color)
 
     # update screen
     pygame.display.flip()
