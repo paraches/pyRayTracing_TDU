5de76c27263844b9326fc927f8383a0dd2e315d7
paraches <paraches+github@gmail.com>
2022-01-15 15:19:09 +0900

    theme 3-1 finished

--- GeometryUtils/GeometryUtil.py
+++ GeometryUtils/GeometryUtil.py
@@ -1,3 +1,6 @@
+C_EPSILON: float = 1.0 / 512.0
+
+
 def map_range(value: float, in_min: float, in_max: float, out_min: float, out_max: float):
     return out_min + (((value - in_min) / (in_max - in_min)) * (out_max - out_min))
 
--- GeometryUtils/Scene.py
+++ GeometryUtils/Scene.py
@@ -1,3 +1,4 @@
+import sys
 from .FColor import FColor
 from .Ray import Ray
 
@@ -8,11 +9,17 @@ class Scene:
         self.light_sources = light_sources
         self.ambient_intensity = ambient_intensity
 
-    def test_intersection_with_all(self, ray: Ray):
-        intersections = [(shape, shape.test_intersection(ray)) for shape in self.shapes if shape.test_intersection(ray) is not None]
-        if len(intersections) == 0:
-            return None, None
-
-        nearest_shape, nearest_intersection = min(intersections, key=lambda intersection: intersection[1].distance)
-
-        return nearest_shape, nearest_intersection
+    def test_intersection_with_all(self, ray: Ray, max_dist: float = sys.float_info.max, exit_once_found: bool = False):
+        nearest_shape = None
+        nearest_intersection = None
+        for shape in self.shapes:
+            intersection = shape.test_intersection(ray)
+            if intersection is not None and intersection.distance < max_dist:
+                if nearest_intersection is None or intersection.distance < nearest_intersection.distance:
+                    nearest_shape = shape
+                    nearest_intersection = intersection
+                    if exit_once_found:
+                        return nearest_shape, nearest_intersection
+        if nearest_intersection is not None:
+            return nearest_shape, nearest_intersection
+        return None, None
